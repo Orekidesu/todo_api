3709be4bc4ab51a69ea73916d83424bd421f6093
Orekidesu <cmcodog10@gmail.com>
2025-08-19 02:12:28 +0800

    Add Task model and relationship to User model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'is_completed',
+        'due_date',
+        'user_id',
+        // 'category_id'
+    ];
+
+
+    protected $casts = [
+        'is_completed' => 'boolean',
+        'due_date' => 'date'
+    ];
+
+
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
