08c08691e99dc04b14d6a11fb21201665550af5c
Orekidesu <cmcodog10@gmail.com>
2025-08-19 13:46:11 +0800

    Add StoreTaskRequest and UpdateTaskRequest for task validation

---
+++ app/Http/Requests/Api/V1/Task/StoreTaskRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\Api\V1\Task;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+            'title' => ['required', 'string', 'max:255'],
+            'description' => ['nullable', 'string'],
+            'due_date' => ['nullable', 'date', 'after_or_equal:today'],
+            'is_completed' => ['required', 'boolean'],
+            'category_id' => [
+                'required',
+                'integer',
+                Rule::exists('categories', 'id')
+            ],
+
+        ];
+    }
+}
