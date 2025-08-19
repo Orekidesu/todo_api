bcd039a3da427acdd8e6abf413c5e3a1f0e78453
Orekidesu <cmcodog10@gmail.com>
2025-08-19 13:50:17 +0800

    Add CategoryController and CategoryResource for category management

---
+++ app/Http/Resources/Api/V1/Category/CategoryResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Api\V1\Category;
+
+use App\Http\Resources\Api\V1\Task\TaskResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'tasks' => TaskResource::collection($this->whenLoaded('tasks'))
+        ];
+    }
+}
